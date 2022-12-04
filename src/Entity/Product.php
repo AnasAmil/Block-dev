69fe0311d084be95bee5d2ec99a946bcbe5d0a3c
Anas Amaneellah <anas@Anass-MacBook-Air.local>
2022-12-04 16:24:00 +0100

    added properties to Warehouse and product

---
+++ src/Entity/Product.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $product_name = null;
+
+    #[ORM\Column]
+    private ?int $quantity = null;
+
+    #[ORM\Column]
+    private ?float $price_unit = null;
+
+    #[ORM\Column]
+    private ?float $mass = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column]
+    private ?int $cell_occupation = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProductName(): ?string
+    {
+        return $this->product_name;
+    }
+
+    public function setProductName(string $product_name): self
+    {
+        $this->product_name = $product_name;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getPriceUnit(): ?float
+    {
+        return $this->price_unit;
+    }
+
+    public function setPriceUnit(float $price_unit): self
+    {
+        $this->price_unit = $price_unit;
+
+        return $this;
+    }
+
+    public function getMass(): ?float
+    {
+        return $this->mass;
+    }
+
+    public function setMass(float $mass): self
+    {
+        $this->mass = $mass;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getCellOccupation(): ?int
+    {
+        return $this->cell_occupation;
+    }
+
+    public function setCellOccupation(int $cell_occupation): self
+    {
+        $this->cell_occupation = $cell_occupation;
+
+        return $this;
+    }
+}
